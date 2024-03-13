324959abbefaf4a1dffec89750571dc2a0c0a763
ghiottoandrea <andreaghiotto9@gmail.com>
2024-03-13 14:30:54 +0100

    Add milestone 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,28 @@
-const {createApp} = Vue
+const { createApp } = Vue
 
 createApp({
-    data(){
-        return{
-            message: `Funzia`
+    data() {
+        return {
+            message: `To Do List`,
+            tasks: [
+                {
+                    text: `Wash the dishes`,
+                    done: true
+                },
+                {
+                    text: `Wash the car`,
+                    done: false
+                }, {
+                    text: `Do the laundry`,
+                    done: true
+                }, {
+                    text: `Watch a film`,
+                    done: false
+                }, {
+                    text: `Make dinner`,
+                    done: false
+                }
+            ]
         }
     }
 }).mount(`#app`)
